851133375e122472120d603c51495698190bb805
Akhil Reddy <avr54@scarletmail.rutgers.edu>
2025-02-13 15:39:49 -0500

    added API functionality -> data coming from MCU Next Movie

---
+++ api.js
@@ -0,0 +1,22 @@
+async function fetchNextMCUMovie() {
+    try {
+        const response = await fetch('ttps://www.whenisthenextmcufilm.com/api');
+        
+        if (!response.ok) {
+            throw new Error('HTTP Error! Status: ${response.status}');
+        }
+
+        const data = await response.json();
+
+        document.getElementById("movie-title").textContent = data.title;
+        document.getElementById("release-date").textContent = `Release Date: ${data.release_date}`;
+        document.getElementById("days-left").textContent = `Days Left: ${data.days_until}`;
+        document.getElementById("poster").src = data.poster_url;
+    }
+    catch (error) {
+        console.error('Error fetching next MCU movie', error);
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", fetchNextMCUMovie);
